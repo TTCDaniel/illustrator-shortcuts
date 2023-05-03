cffcbe7122144a956869daf0f85d753cc24b9246
TTCDaniel <125655855+TTCDaniel@users.noreply.github.com>
2023-05-03 14:26:20 +0200

    Add files via upload

---
+++ Translation DE-IT.jsx
@@ -0,0 +1,175 @@
+﻿var scope = app.activeDocument.pageItems;
+
+var DE_IT = [];
+DE_IT.push(["Vorgehensweise bei der Umrüstung auf das Drema-Werkzeugsystem", "Procedure for changing to drema-tools"]);
+DE_IT.push(["für Maschinen mit Y-Achse oder Monoblockrückseitenwerkzeuge", "FOR STAR"]);
+DE_IT.push(["für Schneideinsätze WNT UltraMini / Dümmel", "For cutting inserts WNT UltraMini / Dümmel"]);
+DE_IT.push(["Einstiche / Gewinde- / Einstichlangdrehen", "Forming, grooving, turning, threading"]);
+DE_IT.push(["Einstiche, Gewinde-, Einstichlangdrehen", "Forming, grooving, turning, threading"]);
+DE_IT.push(["Plan-/Längs-/Konturdrehen / Einstechen", "Turning, copy turning, grooving"]);
+DE_IT.push(["Rückseitenhalter,fest,Überkopf Y-Achse", "Back holder, fixed, single"]);
+DE_IT.push(["HÖHENVERSTELLBARE RÜCKSEITENWERKZEUGE", "HEIGHT ADJUSTABLE BACK SIDE TOOLS"]);
+DE_IT.push(["für schnellste Wechseln und Maschinenrüsten", "For fast changing"]);
+DE_IT.push(["druckbeständig bis mind.", "Pressure up to"]);
+DE_IT.push(["für QER-12 (Quad-Cut) Schneidplatten", "For QER-12 (Quad-Cut) cutting inserts"]);
+DE_IT.push(["Plan-/Längs-/Konturdrehen/Einstechen", "Turning, copy turning, grooving"]);
+DE_IT.push(["für Höheneinstellung Aufsatzhalter", "For adjusting the height of back side tools"]);
+DE_IT.push(["Rückseitenhalter, fest, doppelt", "Back holder, fixed, double"]);
+DE_IT.push(["Rückseitenhalter, fest, einfach", "Back holder, fixed, single"]);
+DE_IT.push(["für CC09../DC11.. Wendeplatten", "For CC09../DC11..cutting inserts"]);
+DE_IT.push(["Rückseitenhalter,fest,doppelt", "Back holder, fixed, double"]);
+DE_IT.push(["Einstiche, Einstichlangdrehen", "Forming, grooving, turning"]);
+DE_IT.push(["Planeinstich/Kontur/Freistich", "Turning, copy turning"]);
+DE_IT.push(["für CC09/DC11 Schneidplatten", "For CC09/DC11 cutting inserts"]);
+DE_IT.push(["für DC11/DC11 Schneidplatten", "For DC11/DC11 cutting inserts"]);
+
+DE_IT.push(["für VC11/DC11 Schneidplatten", "For VC11/DC11 cutting inserts"]);
+DE_IT.push(["Bestellnummer Distanzplatte", "Part number of distance plate"]);
+DE_IT.push(["für Applitec Schneidplatten", "For Applitec cutting inserts"]);
+DE_IT.push(["für Pentacut Schneidplatten", "For pentacut cutting inserts"]);
+DE_IT.push(["Plan-, Längs-, Konturdrehen", "Turning, copy turning"]);
+DE_IT.push(["Bohrer- / Bohrstangenhalter", "Bore holder"]);
+DE_IT.push(["für Typ 312 Schneidplatten", "For type 312 cutting inserts"]);
+DE_IT.push(["Basishalter Klemmeinsätze", "Basic holder clamping inserts"]);
+DE_IT.push(["für Typ312 Schneidplatten", "For type 312 cutting inserts"]);
+DE_IT.push(["Basishalter Klemmeinsätze", "Forming, grooving, turning"]);
+DE_IT.push(["Plan-/Längs-/Konturdrehen", "External- / copy turning"]);
+DE_IT.push(["für WNT / Dümmel Einsätze", "For WNT / Dümmel inserts"]);
+DE_IT.push(["Spannzangenhalter für S20", "Collet holder for S20"]);
+DE_IT.push(["für CC... Schneidplatten", "For CC... cutting inserts"]);
+DE_IT.push(["für DC... Schneidplatten", "For DC... cutting inserts"]);
+DE_IT.push(["für ER... Schneidplatten", "For ER... cutting inserts"]);
+DE_IT.push(["für VC... Schneidplatten", "For VC... cutting inserts"]);
+DE_IT.push(["für WNT / DümmelEinsätze", "For WNT / Dümmel inserts"]);
+DE_IT.push(["Stand: 11. Dezember 2019", ""]);
+DE_IT.push(["Zubehör und Ersatzteile", "Accessories an spare parts"]);
+DE_IT.push(["Stift mit Bund / Pratze", "Pin with flange/cantilever"]);
+DE_IT.push(["für ER16 Schneidplatten", "For ER16 cutting inserts"]);
+DE_IT.push(["für Rundschaftwerkzeuge", "For round shank tools"]);
+DE_IT.push(["für Wendeschneidplatte", "For cutting insert"]);
+DE_IT.push(["Maschinenbezeichnung", "Machine designation"]);
+DE_IT.push(["ZUSÄTZLICHE VORTEILE", "EXTRA ADVANTAGES"]);
+DE_IT.push(["Wendeplattenschraube", "Inserts screw"]);
+DE_IT.push(["Maschinengrundhalter", "Basic holder"]);
+DE_IT.push(["Plan- / Kopierdrehen", "Turning"]);
+DE_IT.push(["Schruppen/Schlichten", "Turning"]);
+DE_IT.push(["Drilling, Ausdrehen", "Drilling, inside turning "]);
+DE_IT.push(["Drehhalter, doppelt", "Turning holder, double"]);
+DE_IT.push(["Einstellvorrichtung", "Adjustment device"]);
+DE_IT.push(["RÜCKSEITENWERKZEUGE", "BACK SIDE TOOLS"]);
+DE_IT.push(["Verschlussschrauben", "Locking screws"]);
+DE_IT.push(["Bohren, Ausdrehen ", "Drilling, inside turning "]);
+DE_IT.push(["Grundhalter-Nummer", "Number of basic holder"]);
+DE_IT.push(["Plan-/Kopierdrehen", "Turning"]);
+DE_IT.push(["für Aufsatzhalter", "For height adjustable back side tools"]);
+DE_IT.push(["Bohren, Ausdrehen", "Drilling, inside turning"]);
+DE_IT.push(["Grundhalternummer", "Number of basic holder"]);
+DE_IT.push(["für Klemmeinsätze", "For clamping inserts"]);
+DE_IT.push(["Kühlmittelverteiler", "Distributor block"]);
+DE_IT.push(["für Hochdruckanschlüsse", "For high pressure"]);
+DE_IT.push(["Kühlmittelaufsatz", "Coolant attachment"]);
+DE_IT.push(["Senkkopfschrauben", "Countersunk bolts"]);
+DE_IT.push(["Plan-/Längsdrehen", "External turning"]);
+DE_IT.push(["FÜR / FOR TSUGAMI", "FOR TSUGAMI"]);
+DE_IT.push(["Sicherungsscheibe", "Locking washer"]);
+DE_IT.push(["Kontur-/Vordrehen", "Contour/Pre-Turning"]);
+DE_IT.push(["Montageanleitung", "Changing procedure"]);
+DE_IT.push(["Spannzangengröße", "Tool clamping"]);
+DE_IT.push(["Distanzplatten", "Distance plates"]);
+DE_IT.push(["Stift mit Bund", "Pin with flange"]);
+DE_IT.push(["Kühlmitteldüse", "Coolant nozzle"]);
+DE_IT.push(["Innenausdrehen", "Inside turning"]);
+DE_IT.push(["Funktionsweise", "Operation"]);
+DE_IT.push(["Aufsatzhalter", "Height adjustable holder"]);
+DE_IT.push(["Distanzplatte", "Distance plate"]);
+DE_IT.push(["Gewindebuchse", "Threaded bush"]);
+DE_IT.push(["Bestellnummer", "Part number"]);
+DE_IT.push(["Gewindedrehen", "Threading"]);
+DE_IT.push(["Klemmeinsatz", "Clamping holder"]);
+DE_IT.push(["Systemaufbau", "Composition"]);
+DE_IT.push(["HM-Unterlage", "HM base"]);
+DE_IT.push(["Grundhalter", "Basic holder"]);
+DE_IT.push(["für Tsugami", "For TSUGAMI"]);
+DE_IT.push(["Drehhalter", "Turning holder"]);
+DE_IT.push(["Hersteller", "Manufacturer"]);
+DE_IT.push(["Ausdrehen", "Forming, grooving, turning"]);
+DE_IT.push(["Alu-Ring", "Aluminium ring"]);
+DE_IT.push(["VORTEILE", "ADVANTAGES"]);
+DE_IT.push(["für Star", "For STAR"]);
+DE_IT.push(["ab Seite", "Page"]);
+DE_IT.push(["Typ 312", "Type 312"]);
+DE_IT.push(["Pratze", "Cantilever"]);
+DE_IT.push(["Bohren", "Drilling"]);
+
+
+function trim(text)
+{
+	return text.replace(/^\s+|\s+$/gm,'');
+}
+
+//trim() is an ECMAScript5 (ES5) feature.
+if(typeof(String.prototype.trim) !== 'function')
+{
+	String.prototype.trim = function trim()
+	{
+		return this.valueOf().replace(/^\s+|\s+$/gm,'');
+	}
+}
+
+
+function logInfo(Txt){
+	var file = new File(Folder.desktop + "/ScriptLog.txt");
+	file.open("w", "TEXT");
+	//move to end
+	//file.seek(0,2);
+	//$.os.search(/windows/i)  != -1 ? file.lineFeed = 'windows'  : file.lineFeed = 'macintosh';
+	file.writeln("\r\n"+Txt+"\r\n");
+	file.close();
+};
+
+var replace = "";
+var changes = 0;
+var log = (new Date).toString();
+var log2 = log;
+for (var i = 0; i < scope.length; i++) {
+
+    var text = scope[i];
+    var scopestring = text.contents;
+
+    if (typeof scopestring  == "string") {
+		log2 += "\r\n\r\noriginal string:\t"+ scopestring;
+        var newstring = "";
+
+        for (var s = 0;s < DE_IT.length; s++) {
+            
+			
+			if(scopestring.trim() == DE_IT[s][0].trim())
+			{
+				//var eng_lowercase = DE_IT[s][1].toLowerCase();
+//				$.writeln(eng_lowercase) ;            //debug.log equivalent
+				//newstring = string.replace(eng_lowercase, "");
+				newstring = scopestring.trim().replace(DE_IT[s][0], DE_IT[s][1]);
+				
+			
+
+            if (newstring != scopestring) {
+				log2 += "\r\nreplaced string:\t"+newstring;
+                changes++;
+
+                var paragraphsArray = newstring.split("\n");
+                text.paragraphs.removeAll();
+                for (var ii = 0; ii < paragraphsArray.length; ii++) {
+					if(paragraphsArray[ii] != "")
+					{
+						text.paragraphs.add(paragraphsArray[ii]);
+					}
+                }
+			}
+            }
+			
+        }
+    }
+	logInfo("\r\n\r\n");
+}
+logInfo(log2);
+alert(changes == 1 ? "1 text object changed" : changes + " text objects changed");
