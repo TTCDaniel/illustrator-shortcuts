77589c520fd372252e747b24066ee7f70e52ee20
TTCDaniel <125655855+TTCDaniel@users.noreply.github.com>
2023-07-14 10:39:09 +0200

    Add files via upload
    \uFF1A Escaped character. Matches a "：" character (char code 65306).
    \u003A Escaped character. Matches a ":" character (char code 58).
    \u2B1C Escaped character. Matches a "⬜" character (char code 11036).
    \u00D8 Escaped character. Matches a "Ø" character (char code 216).
    
    removed \d as duplicated of \u0030-\u0039

--- Font-Text-Related/AI_GrepReplaceChinese.jsx
+++ Font-Text-Related/AI_GrepReplaceChinese.jsx
@@ -1,8 +1,22 @@
 var scope = app.activeDocument.selection.length ? app.activeDocument.selection : app.activeDocument.pageItems;
 
-var find = "([^\u0000-\u001F\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E\u0030-\u0039\u0041-\u005A\u0061-\u007A\d])";
+var find = "([^\uFF1A\u003A\u2B1C\u00D8\u00B1\u0000-\u001F\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E\u0030-\u0039\u0041-\u005A\u0061-\u007A])";
+/*
+\uFF1A Escaped character. Matches a "：" character (char code 65306).
+\u003A Escaped character. Matches a ":" character (char code 58).
+\u2B1C Escaped character. Matches a "⬜" character (char code 11036).
+\u00D8 Escaped character. Matches a "Ø" character (char code 216).
+\u00B1 Escaped character. Matches a "±" character (char code 177).
+\u0000-\u001F Range. Matches a character in the range NULL to US (char code 0 to 31). Case sensitive.
+\u0020-\u002F Range. Matches a character in the range SPACE to "/" (char code 32 to 47). Case sensitive.
+\u003A-\u0040 Range. Matches a character in the range ":" to "@" (char code 58 to 64). Case sensitive.
+\u005B-\u0060 Range. Matches a character in the range "[" to "`" (char code 91 to 96). Case sensitive.
+\u007B-\u007E Range. Matches a character in the range "{" to "~" (char code 123 to 126). Case sensitive.
+\u0030-\u0039 Range. Matches a character in the range "0" to "9" (char code 48 to 57). Case sensitive. [0-9] equivalent
+\u0041-\u005A Range. Matches a character in the range "A" to "Z" (char code 65 to 90). Case sensitive. [A-Z] equivalent
+\u0061-\u007A Range. Matches a character in the range "a" to "z" (char code 97 to 122). Case sensitive. [a-z] equivalent
 
-
+*/
     var replace = "";
  
 
