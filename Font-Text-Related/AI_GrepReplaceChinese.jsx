4ec8978c56a0d47d210085e598d725262285baa0
TTCDaniel <125655855+TTCDaniel@users.noreply.github.com>
2023-11-22 11:31:51 +0100

    Ø codificato come \u00F06E ()
    Ø codificato come \u00F06E () ma non riconosciuto

--- Font-Text-Related/AI_GrepReplaceChinese.jsx
+++ Font-Text-Related/AI_GrepReplaceChinese.jsx
@@ -1,10 +1,12 @@
 var scope = app.activeDocument.selection.length ? app.activeDocument.selection : app.activeDocument.pageItems;
-
 var find = "([^\u00B0\uFF1A\u2B1C\u00D8\u00B1\u0000-\u001F\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E\u0030-\u0039\u0041-\u005A\u0061-\u007A])";
 /*
+ATTENZIONE
+il carattere Ø viene spesso decodificato come \u00F06E () ma non viene correttamente identificato 
+quindi .replace(new RegExp('\u00F06E', 'g'), "Ø") non funziona
+*/
 
-
-
+/*
 \u00B0 Escaped character. Matches a "°" character (char code 176).
 \u00B1 Escaped character. Matches a "±" character (char code 177).
 \u00D8 Escaped character. Matches a "Ø" character (char code 216).
@@ -18,9 +20,8 @@ var find = "([^\u00B0\uFF1A\u2B1C\u00D8\u00B1\u0000-\u001F\u0020-\u002F\u003A-\u
 \u0030-\u0039 Range. Matches a character in the range "0" to "9" (char code 48 to 57). Case sensitive. [0-9] equivalent
 \u0041-\u005A Range. Matches a character in the range "A" to "Z" (char code 65 to 90). Case sensitive. [A-Z] equivalent
 \u0061-\u007A Range. Matches a character in the range "a" to "z" (char code 97 to 122). Case sensitive. [a-z] equivalent
-
 */
-    var replace = "";
+    var replace = ""
  
 
         var changes = 0;
@@ -29,7 +30,7 @@ var find = "([^\u00B0\uFF1A\u2B1C\u00D8\u00B1\u0000-\u001F\u0020-\u002F\u003A-\u
 
             var text = scope[i];
 
-            var string = text.contents;  
+            var string = text.contents; 
             if(typeof string == "string"){
                 var newstring = string.replace( new RegExp(find, 'g'), replace);
                 if (newstring != string) {
@@ -42,5 +43,4 @@ var find = "([^\u00B0\uFF1A\u2B1C\u00D8\u00B1\u0000-\u001F\u0020-\u002F\u003A-\u
                 }
             }
         }
-        alert( changes==1 ? "1 text object changed" : changes + " text objects changed");
-
+        alert( changes + " Testi modificati\r\nAssicurati che i siano rimasti tutti caratteri grafici necessari");
